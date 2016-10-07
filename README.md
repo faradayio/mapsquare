cb738eaa81c9c1cae1105512bec5fe1a50f5ecad
Bill Morris <wboykinm@geosprocket.com>
2016-10-07 17:03:01 -0400

    add gist export support

--- README.md
+++ README.md
@@ -9,17 +9,19 @@ _Get GeoJSON from the [Foursquare venues API](https://developer.foursquare.com/d
 
 ## Usage
 
-`mapsquare -l <placename> -q <venue keyword query> -a <Foursquare authorization token>`
+`mapsquare -l <placename> -q <venue keyword query> -a <Foursquare authorization token> -g`
 
 ### Arguments
 
 * `-l, --location` A placename for the venue search (e.g. 'Salt Lake City, UT')
 * `-q, --query` A venue type keyword (e.g. 'hardware' or 'movie theater')
 * `-a, --auth` Foursquare authorization token. [Get a convenience token for testing here](https://developer.foursquare.com/docs/explore#req=venues/explore%3Fnear%3DSalt+Lake+City,+UT%26query%3Dpark) (Foursquare login or signup required).
+* `-g, --gist` (OPTIONAL) Flag to output results as an anonymous gist
 
 ### Notes
 
-- Limited to 50 venues per request - Foursquare will choose these based on relevance
+- Limited to 50 venues per request - Foursquare will choose these based on ratings and relevance, not necessarily by proximity
+- Be sure to fork the gist outputs to your own github account
 
 ## License
 
--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 "use strict"
 let fs = require('fs')
 let request = require('request')
+let GitHubApi = require("github")
 let commandLineArgs = require('command-line-args')
  
 let optionDefinitions = [
   { name: 'location', alias: 'l', type: String },
   { name: 'query', alias: 'q', type: String },
   { name: 'auth', alias: 'a', type: String },
-  //{ name: 'gist', alias: 'g', type: Boolean, defaultValue: false }
+  { name: 'gist', alias: 'g', type: Boolean, defaultValue: false }
 ]
 
 let options = commandLineArgs(optionDefinitions)
@@ -17,11 +18,25 @@ let options = commandLineArgs(optionDefinitions)
 let near = options.location
 let query = options.query
 let authToken = options.auth
-//let gistFlag = options.gist
+let gistFlag = options.gist
+
+let github = new GitHubApi({
+  // optional args 
+  //debug: true
+  //protocol: "https",
+  //host: "github.my-GHE-enabled-company.com", // should be api.github.com for GitHub 
+  //pathPrefix: "/api/v3", // for some GHEs; none for GitHub 
+  //headers: {
+      //"user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent 
+  //},
+  //Promise: require('bluebird'),
+  //followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects 
+  //timeout: 5000
+});
 
 let exploreUrl = 'https://api.foursquare.com/v2/venues/explore?limit=50&near=' + near + '&query=' + query + '&oauth_token=' + authToken + '&v=20161005'
 
-let outGeojson = {type: 'FeatureCollection', features: []}
+let outGeoJson = {type: 'FeatureCollection', features: []}
 
 request(exploreUrl, function (error, response, body) {
   if (!error && response.statusCode == 200) {
@@ -29,7 +44,7 @@ request(exploreUrl, function (error, response, body) {
     let items = payload.response.groups[0].items
     for (var i = 0; i < items.length; i++) {
       let markerColor = items[i].venue.ratingColor || 'A8A7A9'
-      outGeojson.features.push({
+      outGeoJson.features.push({
         type: 'Feature', 
         properties: {
           name: items[i].venue.name || '',
@@ -51,11 +66,31 @@ request(exploreUrl, function (error, response, body) {
         }
       })
     }
-    //if (gistFlag === true) {
-      //console.log('Anonymous gist posted to XXXXXXX')
-    //} else {
-      fs.writeFileSync('foursquare_' + payload.response.geocode.slug + '_' + query + '.geojson', JSON.stringify(outGeojson, null, 2))
-      console.log('Output file: "foursquare_' + payload.response.geocode.slug + '_' + query + '.geojson"')
-    //}
+    let fileName = 'foursquare_' + payload.response.geocode.slug + '_' + query + '.geojson'
+    if (gistFlag === true) {
+      let gistPost = {
+        "description": "Foursquare venues: " + query + " in " + near,
+        "public": true,
+        "files": {}
+      }
+      gistPost.files[fileName] = {}
+      gistPost.files[fileName]['content'] = JSON.stringify(outGeoJson)
+      
+      //console.log(JSON.stringify(gistPost))
+      github.gists.create(
+        gistPost, 
+        function(err, rest) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(rest.html_url);
+            //console.log('You GeoJSON is available in this anonymous gist: ')
+          }
+        }
+      )
+    } else {
+      fs.writeFileSync(fileName, JSON.stringify(outGeoJson, null, 2))
+      console.log('Output file: "' + fileName + '"')
+    }
   }
 })
